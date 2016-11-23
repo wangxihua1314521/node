2dff36982cbdd399c62bf98756f3571540702ab7
wangxihua1314521 <842726562@qq.com>
2016-11-23 20:33:49 +0800

    node

---
+++ imooc/nodejs01/4_3/index.js
@@ -0,0 +1,15 @@
+//由于class是关键字 所以这里是klass
+var klass = require('./class');
+
+
+klass.add('Scoot', ['白富美', '高富帅']);
+
+// exports.add = function(klasses) {
+//     klasses.forEach(function(item, index) {
+//         var _klass = item;
+//         var teacherName = item.teacherName;
+//         var students = item.students;
+//
+//         klass.add(teacherName, students);
+//     });
+// };
